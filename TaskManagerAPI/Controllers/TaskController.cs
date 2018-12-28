2c58fd7e60fff95636bfc3b1aa652298087cadb7
VaradharajanSubramani <Varadharajanvbm@gmail.com>
2018-12-28 12:48:16 +0000

    New project

---
+++ TaskManagerAPI/Controllers/TaskController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TaskManagerAPI.Repository;
+
+namespace TaskManagerAPI.Controllers
+{    
+    public class TaskController : ApiController
+    {
+        private TaskRepository TaskRep = new TaskRepository();
+        // GET: api/Task
+        [HttpGet]
+        public IList<Task> GetTasks()
+        {
+            return TaskRep.GetAllTasks();
+        }
+
+        // GET: api/Task/5
+        [HttpGet]
+        public Task GetTaskDetails(int id)
+        {
+            return TaskRep.GetTaskDetail(id);
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public void SaveTask([FromBody]Task value)
+        {
+            TaskRep.SaveTask(value);
+        }
+
+        // PUT: api/Task/5
+        [HttpPut]
+        public void UpdateTask(int id, [FromBody]Task value)
+        {
+            TaskRep.UpdateTask(id, value);
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete]
+        public void DeleteTask(int id)
+        {
+            TaskRep.DeleteTask(id);
+
+        }
+
+        [HttpPut]        
+        public void EndTask(int id)
+        {
+            TaskRep.EndTask(id);
+           // return  StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ TaskManagerAPI/Repository/TaskRepository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerAPI.Repository
+{
+    public class TaskRepository
+    {
+        private TaskManagerEntities TME = new TaskManagerEntities();
+
+         public List<Task> GetAllTasks()
+        {
+            List<Task> tasklist = TME.Tasks.ToList();
+
+            return tasklist;
+        }
+
+        public int SaveTask( Task taskdetails)
+        {
+            try
+            {
+                if (taskdetails != null)
+                {
+                    TME.Tasks.Add(taskdetails);
+                    return TME.SaveChanges();
+                }               
+            }
+            catch (Exception e)
+            {
+                throw e;               
+            }
+
+            return 0;
+        }
+
+        public int UpdateTask(int taskid, Task taskdetails)
+        {
+            try
+            {
+                if (taskdetails != null)
+                {
+                    if (taskdetails.Task_ID != 0)
+                    {
+                        Task t = TME.Tasks.Where(x => x.Task_ID == taskid).FirstOrDefault();
+                        TME.Tasks.Add(t);
+                        TME.Entry(t).State = System.Data.Entity.EntityState.Modified;
+                        return TME.SaveChanges();
+
+                    }
+                    return TME.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return 0;
+        }
+
+        public void DeleteTask(int taskid)
+        {
+            try
+            {
+                if(taskid!=0)
+                {
+                    Task t = TME.Tasks.Where(x => x.Task_ID == taskid).FirstOrDefault();
+                    TME.Tasks.Remove(t);
+                    TME.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }           
+        }
+
+        public int EndTask(int taskid)
+        {
+            try
+            {
+                if(taskid != 0)
+                {
+                    Task t = TME.Tasks.Where(x => x.Task_ID == taskid).FirstOrDefault();
+                    t.IsCompleted = true;
+                    return TME.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return 0;
+        }
+
+        public Task GetTaskDetail(int taskid)
+        {
+            try
+            {
+                if (taskid != 0)
+                {
+                    Task t = TME.Tasks.Where(x => x.Task_ID == taskid).FirstOrDefault();
+                    return t;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
+        }
+    }
+}
